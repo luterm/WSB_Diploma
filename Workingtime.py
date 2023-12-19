f71475a372e21e8c1f8cd708a27da38b769cf7fd
Marcin Lutrosinski <152770986+luterm@users.noreply.github.com>
2023-12-19 19:19:48 +0100

    Add files via upload

---
+++ Workingtime.py
@@ -0,0 +1,33 @@
+
+# 2nd version
+user_hours = (input("Enter hours:"))
+user_min = (input("Enter minutes:"))
+
+print(type(user_hours))
+print(type(user_min))
+
+user_hours = int(user_hours)
+user_min = int(user_min)
+
+print(type(user_hours))
+print(type(user_min))
+
+while user_min >= 60:
+    user_hours += 1
+    user_min -= 60
+
+print("Working time:", user_hours, "hours and", user_min, "minutes.")
+quit()
+
+
+# basic version
+user_hours = (1)
+user_min = (80)
+
+while user_min >= 60:
+    user_hours += 1
+    user_min -= 60
+
+work = (user_hours, user_min)
+
+print (work)
